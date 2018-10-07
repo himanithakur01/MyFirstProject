9af13d2ae4a7acecdc7297189c4cbb48f053da2c
nidhisnaa@gmail.com <nidhisnaa@gmail.com>
2018-10-07 10:13:51 +0530

    First commit

--- controller.js
+++ controller.js
@@ -3,6 +3,18 @@ var app= angular.module("myShoppingList" ,[]);
 app.controller("myCtrl", function($scope){
     $scope.products =["Milk" , "Bread" ,"Cheese"];
     $scope.addItem =function(){
-        $scope.products.push($scope.addMe);
+        $scope.errortext= "";
+        if(!$scope.addMe){return};
+        if($scope.products.indexOf($scope.addMe) == -1){
+            $scope.products.push($scope.addMe);
+        }
+        else{
+            $scope.errortext= "Already in list" ;
+        }
+        
     }
+    $scope.removeItem = function(x){
+        $scope.products.splice(x,1);
+    }
+    
     });
